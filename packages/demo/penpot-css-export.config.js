878d18dcb2f6e0597b29816c42295588384b8518
Roberto Redradix <roberto@redradix.com>
2023-08-22 11:36:13 +0200

    chore(demo): update Penpot file id

--- packages/demo/penpot-css-export.config.js
+++ packages/demo/penpot-css-export.config.js
@@ -8,7 +8,7 @@ const config = {
   pages: [
     {
       output: 'src/styles/ui.css', // 👈🏻 Path where your css should be generated.
-      fileId: '71b1702b-2eb1-81d6-8002-f82a5f182087',
+      fileId: 'abea3ef6-4c19-808a-8003-01370d9cb586',
       pageId: '71b1702b-2eb1-81d6-8002-f82a5f182088',
     },
   ],
